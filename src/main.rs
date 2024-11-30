078ccabc48e7768dd607e572c7a3a136a529fc2e
Jaeson Fan <sergefan@live.com>
2024-12-01 03:08:12 +0800

    fix breaking changes (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod app;
 
 use app::*;
+use leptos::prelude::mount_to_body;
 use leptos::*;
-use thaw::MessageProvider;
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! {
-            <MessageProvider>
                 <App/>
-            </MessageProvider>
         }
     })
 }
