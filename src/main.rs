c34d1b718d92e0fc2d96e2c61c03f6185ab1440a
Jaeson Fan <sergefan@live.com>
2024-12-11 20:19:56 +0800

    wrap App with ToasterProvider

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod app;
 use app::*;
 use leptos::prelude::mount_to_body;
 use leptos::*;
+use thaw::ToasterProvider;
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! {
+            <ToasterProvider>
                 <App/>
+            </ToasterProvider>
         }
     })
 }
